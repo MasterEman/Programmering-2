2284235374006a9a1af2e7acfe42152839da36cd
MasterEman <emanuel.kulich@gmail.com>
2018-08-31 13:58:11 +0200

    v35 Uppgift 2

--- src/uppgifterV35/stringReverse.java
+++ src/uppgifterV35/stringReverse.java
@@ -2,27 +2,20 @@ package uppgifterV35;
 
 public class stringReverse {
 	
-			 String reverse = "hej, och, hejdå";
-	
-			 public String reverseString(String str) {
-	
-			 if (str.length() == 1) {
-				 return str;
-			 }
-			 
-			 else {
-				 reverse += str.charAt(str.length()-1)
-		
-				 +reverseString(str.substring(0,str.length()-1));
-		
-				 return reverse;	
-			 	}	
-			 }
-	
-			 public static void main(String a[]) {
-	
-				 stringReverse srr = new stringReverse();
-		
-				 System.out.println(srr.reverseString(""));
-			 }
+		public static void main(String[] args) {
+				
+				System.out.println(reverse("Hej JaoK"));	
+			}
+			
+			public static String reverse(String input) {
+				
+				if (input.length() == 1) {
+					
+					return input;
+				}
+				
+				else {
+					return reverse(input.substring(1))+ input.charAt(0);
+				}
+			}
 		}
