9fb98e71495a9a637059d49c9fc5a85a468971bd
MasterEman <emanuel.kulich@gmail.com>
2018-08-31 14:02:39 +0200

    v35 Uppgift 3

---
+++ src/uppgifterV35/unknownObjects.java
@@ -0,0 +1,16 @@
+package uppgifterV35;
+
+public class unknownObjects {
+
+		public static void main(String[] args) {
+				
+				generic();
+			}
+			
+			@SuppressWarnings("unchecked")
+			public static <T> T[] generic(T... generic) {
+				
+				return generic;
+		
+	}
+}
