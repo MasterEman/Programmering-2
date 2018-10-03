0b2c53c187c91fb1b42061e37b8872bbbb79e1d0
MasterEman <emanuel.kulich@gmail.com>
2018-10-03 10:29:49 +0200

    Maze 6

--- src/MazeSolver/MapSolver.java
+++ src/MazeSolver/MapSolver.java
@@ -1,34 +1,84 @@
 package MazeSolver;
 
-/*import blocks.Block;
+import MazeSolver.Block;
+import MazeSolver.FullBlock;
+import MazeSolver.GoalBlock;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 
-	public class MapSolver extends Block {
-
-		MapSolver(int startX, int startY){
+	public class MapSolver {
+		
+		private Map map;
+		private int steps = 0;
+		private boolean solution = false;
+	
+		public MapSolver(Map map ){
+			this.map = map;
 			
-		}
+			
+			int x = map.startX();
+			int y = map.startY();
+			
+			solve (x, y, 1); //right
+			solve (x, y, 2); //up
+			solve (x, y, 3); //left
+			solve (x, y, 4); //down
+			}
 		
-		public void update() {
+			public void solve(int x, int y, int dir){
+	
 				
+				Block b = map.getBlock(x,y);
+				if (b == null || b instanceof FullBlock){
+					return;
+				}
+				if (b instanceof GoalBlock ){
+					solution = true;
+				}
+				
+				Circle cir = new Circle(Block.SIZE/2,Block.SIZE/2,Block.SIZE/3,Color.YELLOW);
+				
+				b.getChildren().add(cir);
+				
+				if (solution == true){
+					return;
+				}
+				
+				
+				if (dir == 1){ //right
+					
+					solve (x+1, y, 1); 
+					solve (x+1, y, 2); 
+					//solve (x, y, 3); 
+					solve (x+1, y, 4); 
+				}
+				
+				if (dir == 2){ //up
+					
+					solve (x, y-1, 1); 
+					solve (x, y-1, 2); 
+					solve (x, y-1, 3); 
+					//solve (x+1, y, 4); 
+				}
+				
+				if (dir == 3){ //left
+					
+					//solve (x+1, y, 1); 
+					solve (x-1, y, 2); 
+					solve (x-1, y, 3); 
+					solve (x-1, y, 4); 
+				}
+				
+				if(dir == 4){ //down
+					
+					solve (x, y+1, 1); 
+					//solve (x, y+1, 2); 
+					solve (x, y+1, 3); 
+					solve (x, y+1, 4); 
+				}
+				
+				if (!solution){
+					b.getChildren().remove(cir);
+				}		
+			}				
 		}
-	}*/
-
-public class MapSolver {
-
-	public MapSolver(Map map) {
-	
-		int x = map.startX();
-		int y = map.startY();
-		
-		solve(x, y, 1); // 1 = right
-		solve(x, y, 2); // 2 = up
-		solve(x, y, 3); // 3 = left
-		solve(x, y, 4); // 4 = down	
-	}
-	
-	public static void solve(int x, int y, int dir) {
-	
-		//*insert* kod som löser maze:en lmao
-		
-	}
-}
--- src/MazeSolver/maze.java
+++ src/MazeSolver/maze.java
@@ -5,15 +5,15 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class maze extends Application{
+public class maze extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 
-		Map map = MapInterpreter.interpretMap(new File("src/maze/map.txt"));
+		Map map = MapInterpreter.interpretMap(new File("src/mazesolver/map.txt"));
 		Scene scene = new Scene(map,map.getWidth(),map.getHeight());
 		
-		primaryStage.setTitle("Maze");
+		primaryStage.setTitle("Maze Solving AI");
 		primaryStage.setResizable(true);
 		primaryStage.setScene(scene);
 		primaryStage.show();
