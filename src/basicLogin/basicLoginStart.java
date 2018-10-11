4d4b2e7a3455b2f02403b4febeb17c97b8eabc37
MasterEman <emanuel.kulich@gmail.com>
2018-10-11 10:40:47 +0200

    Updated

--- src/basicLogin/basicLoginStart.java
+++ src/basicLogin/basicLoginStart.java
@@ -1,13 +1,14 @@
 package basicLogin;
 
-import java.awt.TextField;
-
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class basicLoginStart extends Application {
@@ -22,26 +23,28 @@ public class basicLoginStart extends Application {
 
 	public void start(Stage primaryStage) throws Exception {
 		
-		scene = new Scene(new Group(), 400, 400);
-		window = primaryStage;
-	
-		TextField usernameInput = new TextField();
-		TextField passwordInput = new TextField();
+		scene = new Scene(new Group(), 400, 400, Color.LIGHTGREY);
+		Group root = new Group();
 		
-		button = new Button("Submit");
-		button.setOnAction( e -> System.out.println("Username: " + usernameInput.getText() + " " + "Password: " + passwordInput.getText()));
+		window = primaryStage;
 		
 		VBox layout = new VBox(10);
 		layout.setPadding(new Insets(20, 20, 20, 20));
 		
-		layout.getChildren().addAll();
+		TextField userInput = new TextField();
+		TextField passInput = new TextField();
 		
+		button = new Button("Submit");
+		button.setOnAction( e -> System.out.println("Username: " + userInput.getText() + " " + "Password: " + passInput.getText()));
+		
+		root.getChildren().addAll(userInput);
+		root.getChildren().addAll(passInput);
+		root.getChildren().addAll(button);
+		
+		window.getIcons().add(new Image("file:icon.jpg"));
 		window.setResizable(false);
 		window.setScene(scene);
 		window.setTitle("Login");
 		window.show();
 	}
 }
-
-
-
