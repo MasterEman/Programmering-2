a7f0208c2c46c84c704933d96a961cf400b9ff79
MasterEman <emanuel.kulich@gmail.com>
2018-10-12 13:28:39 +0200

    update

--- src/chessGame/chessBoard.java
+++ src/chessGame/chessBoard.java
@@ -1,5 +1,23 @@
 package chessGame;
 
 public class chessBoard {
-
+	
+	public chessBoard() {
+		
+		for (int i = 1; i < 8; i++) {
+			for (int j = 1; j < 8; j++) {
+				
+				whiteSquare ws;
+				blackSquare bs;
+				
+				ws = new whiteSquare();
+				ws.setTranslateX(j);
+				ws.setTranslateY(i);
+				
+				bs = new blackSquare();
+				bs.setTranslateX(j);
+				bs.setTranslateY(i);
+			}
+		}
+	}
 }
--- src/chessGame/chessGameStart.java
+++ src/chessGame/chessGameStart.java
@@ -1,6 +1,7 @@
 package chessGame;
 
 import javafx.application.Application;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
@@ -8,11 +9,15 @@ import javafx.stage.Stage;
 public class chessGameStart extends Application {
 	
 	public void start(Stage primaryStage) throws Exception {
-				
+		
+		Group root = new Group();
+		
+		Scene scene = new Scene(root, square.SIZE  * 8, square.SIZE  * 8);
+						
 		primaryStage.getIcons().add(new Image("file:icon.jpg"));
 		primaryStage.setTitle("Chess Game");
 		primaryStage.setResizable(false);
-//		primaryStage.setScene(scene);
+		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
 	
