035e2f0ff337a646dfd9c2086e501a2908f21c82
Emanuel Kulich <emanuel@kulich.me>
2019-04-25 09:58:51 +0200

    update

--- src/bankAccount/bankAccountStart.java
+++ src/bankAccount/bankAccountStart.java
@@ -1,17 +1,50 @@
 package bankAccount;
 
+import java.util.Scanner;
+
 import bankAccount.bankAccount;
 
 public class bankAccountStart {
+	
+	String fullName;
+	
+	private static Scanner scr;
+
+	private static int accountNumber;
 
 	public static void main (String[] args) {
 		
-		bankAccount ba0 = new bankAccount("Emanuel Kulich", 1, 1337.69);
-		System.out.println("--------------------------------------------------------------------");
-		System.out.println("Welcome to your account, " + ba0.getName());
+		scr = new Scanner(System.in);
+		
+		System.out.println("---------------------------------------------------------");
+		System.out.println("Welcome!");
 		System.out.println(" ");
-		System.out.println("Account Number: " + ba0.getAccountNumber());
-		System.out.println("Balance: " + ba0.getBalance());
-		System.out.println("--------------------------------------------------------------------");
+		System.out.println("If you have an account, please enter your details below: ");
+		System.out.println("Full Name: ");
+		String fullName = scr.nextLine();
+		System.out.println("Account Number: ");
+		setAccountNumber(scr.nextInt());
+		
+		
+		
+		if (fullName != null) {
+		
+			bankAccount ba0 = new bankAccount("Emanuel Kulich", 1, 1337.69);
+			System.out.println("---------------------------------------------------------");
+			System.out.println("Welcome to your account, " + ba0.getName());
+			System.out.println(" ");
+			System.out.println("Account Number: " + ba0.getAccountNumber());
+			System.out.println("Balance: " + ba0.getBalance());
+			System.out.println("---------------------------------------------------------");
+			
+		}
+	}
+
+	public static int getAccountNumber() {
+		return accountNumber;
+	}
+
+	public static void setAccountNumber(int accountNumber) {
+		bankAccountStart.accountNumber = accountNumber;
 	}
 }
